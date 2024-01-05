460e7e6186a4abdfbdf2034ada42b640c644fdb7
Daniel Almachi <ddalmach@pichincha.com>
2024-01-05 11:44:47 -0500

    Add logs

--- src/main/java/com/bp/test/authentication/controller/AuthController.java
+++ src/main/java/com/bp/test/authentication/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.bp.test.authentication.service.AuthenticationService;
 import com.bp.test.authentication.service.dto.AuthenticationRequest;
 import com.bp.test.authentication.service.dto.AuthenticationResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final AuthenticationService service;
     @PostMapping("/authenticate")
     public AuthenticationResponse authenticate(@RequestBody AuthenticationRequest request){
+        log.info("Authenticating user: {}",request.username());
         return service.authenticate(request);
     }
 }
--- src/main/java/com/bp/test/authentication/service/JwtService.java
+++ src/main/java/com/bp/test/authentication/service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -12,10 +13,12 @@ import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class JwtService {
     private static final String SECRET_KEY = "53494F4F6B4C66617443526135504E767873304D67736335624C477362676A39";
 
     public String generateToken(String username) {
+        log.info("Generating token for user: {}", username);
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
